cebc3e685b28c65ae981efe71c31d6fca1a64456
jirapatmalai <jtm@jmandjm.com>
2021-03-21 15:29:01 +0700

    update conditions.

--- src/models/areaCondition/index.ts
+++ src/models/areaCondition/index.ts
@@ -1,40 +1,33 @@
-const mongoose = require('mongoose')
-const mongoosePaginate = require("mongoose-paginate")
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 const schema = new Schema({
   name: String,
   area: { type: ObjectId, ref: "Area" },
 
-  oerationDate:Date,
-  operationYear:Number,
-  calendarYear:Number,
-  contractYear:Number,
+  oerationDate: Date,
+  operationYear: Number,
+  calendarYear: Number,
+  contractYear: Number,
 
-  conditions: [{
-    category: String,
-    value: String,
-    valueProfit: String,
-    valueLoss: String,
-    value2: String,
-    description: String,
-    collector: String,
-    calculation: String,
-    period: String,
-}],
+  conditions: [
+    {
+      category: String,
+      contributionPercent: Number,
+      contributionLimit: Number,
+      profitType: Number,
+      lossType: Number,
+      description: String,
+      collector: String,
+      calculation: String,
+      period: String,
+    },
+  ],
 
-
-  category: String,
-  contributionPercent: Number,
-  contributionLimit: Number,
-  profitType: Number,
-  lossType: Number,
-  description: String,
-  collector: String,
-  calculation: String,
-  period: String,
-
-})
-schema.plugin(mongoosePaginate)
-const AreaCondition = mongoose.model("AreaCondition", schema)
-export default AreaCondition
+  maximum: Number,
+  isMaximum: Boolean,
+});
+schema.plugin(mongoosePaginate);
+const AreaCondition = mongoose.model("AreaCondition", schema);
+export default AreaCondition;
