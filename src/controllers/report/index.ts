b550b993520adf52981557f241083afa84c27d6a
Jirapat Malai <jtm@jmandjm.com>
2021-03-22 15:01:48 +0700

    update.

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
 import Usage from '../../models/usage/index'
 import Invoice from '../../models/invoice/index'
+import Receipt from '../../models/receipt/index'
 import History from '../../models/history/index'
 import mongoose from "mongoose";
 
@@ -13,6 +14,15 @@ export const getDebtByMeter = (req: Request, res: Response) => {
 export const getDebtByInvoice = (req: Request, res: Response) => {
   let list = req.body.list
   Invoice.find({ _id: { $in: list }}).lean().then((docs:any)=>{
+    docs.forEach((element:any,i:number) => {
+      docs[i].debt = "ทดสอบหนี้"
+    });
+    res.send(docs)
+  })
+}
+export const getDebtByReceipt = (req: Request, res: Response) => {
+  let list = req.body.list
+  Receipt.find({ _id: { $in: list }}).lean().then((docs:any)=>{
     docs.forEach((element:any,i:number) => {
       docs[i].debt = "ทดสอบหนี้"
     });
--- src/routers/report/index.ts
+++ src/routers/report/index.ts
@@ -6,4 +6,5 @@ let endpoints = "reports"
 module.exports = (app: Express) => {
   app.post(`/api/v1/${endpoint}-debt-meter/`, ctrl.getDebtByMeter)
   app.post(`/api/v1/${endpoint}-debt-invoice/`, ctrl.getDebtByInvoice)
+  app.post(`/api/v1/${endpoint}-debt-receipt/`, ctrl.getDebtByReceipt)
 }
