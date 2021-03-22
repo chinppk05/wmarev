65a5d5fdfb67a8627428dcce619fa2035b04387d
Jirapat Malai <jtm@jmandjm.com>
2021-03-22 15:30:31 +0700

    update

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -4,19 +4,27 @@ import Invoice from '../../models/invoice/index'
 import Receipt from '../../models/receipt/index'
 import History from '../../models/history/index'
 import mongoose from "mongoose";
+import luxon, { DateTime } from "luxon";
 
 export const getDebtByMeter = (req: Request, res: Response) => {
   let meter = req.body.meter
   let list = req.body.list
-  Invoice.find({ _id: { $in: list }})
+  Invoice.find({ _id: { $in: list } })
   res.send({})
 }
 export const getDebtByInvoice = (req: Request, res: Response) => {
   let list = req.body.list
-  Invoice.find({ _id: { $in: list }}).lean().then((docs:any)=>{
-    Invoice.find({meter:{$in:docs.map((el:any)=>el.meter)}}).lean().then((founds:any)=>{
-      docs.forEach((item:any,i:number) => {
-        docs[i].debtArray = founds.filter((el:any)=>el.meter==item.meter)
+  Invoice.find({ _id: { $in: list } }).lean().then((docs: any) => {
+    Invoice.find({ meter: { $in: docs.map((el: any) => el.meter) } }).lean().then((founds: any) => {
+      docs.forEach((item: any, i: number) => {
+        let debtArray = founds.filter((el: any) => el.meter == item.meter)
+        debtArray = debtArray.map((el: any) => {
+          return {
+            ...el,
+            dt:mo(el.year,el.month)
+          }
+        })
+        docs[i].debtArray = founds.filter((el: any) => el.meter == item.meter)
         docs[i].debtText = "ทดสอบหนี้"
         docs[i].debtAmount = 299.50
       });
@@ -26,8 +34,8 @@ export const getDebtByInvoice = (req: Request, res: Response) => {
 }
 export const getDebtByReceipt = (req: Request, res: Response) => {
   let list = req.body.list
-  Receipt.find({ _id: { $in: list }}).lean().then((docs:any)=>{
-    docs.forEach((element:any,i:number) => {
+  Receipt.find({ _id: { $in: list } }).lean().then((docs: any) => {
+    docs.forEach((element: any, i: number) => {
       docs[i].debtText = "ทดสอบหนี้"
       docs[i].debtAmount = 399.75
     });
@@ -35,7 +43,14 @@ export const getDebtByReceipt = (req: Request, res: Response) => {
   })
 }
 
-let debt1 = new Promise((resolve,reject)=>{
-  
+let mo = (year: number, month: number) => {
+  return DateTime.fromObject({
+    year,
+    month
+  })
+}
+
+let debt1 = new Promise((resolve, reject) => {
+
   resolve("")
 })
