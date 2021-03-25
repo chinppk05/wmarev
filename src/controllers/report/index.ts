d3c9240ce5f1e25e6138979f2d24588f56886c48
Jirapat Malai <jtm@jmandjm.com>
2021-03-25 08:03:08 +0700

    update.

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -56,7 +56,7 @@ export const getCustomerLatest = (req: Request, res: Response) => {
 export const getDebtByReceipt = (req: Request, res: Response) => {
   let list = req.body.list
   Receipt.find({ _id: { $in: list } }).lean().then((originals: any) => {
-    let docs = Object.assign({},originals)
+    let docs = JSON.parse(JSON.stringify(originals))
     Invoice.find({ meter: { $in: docs.map((el: any) => el.meter) }, isPaid: false }).sort("-year -month").lean().then((founds: any) => {
       docs.forEach((item: any, i: number) => {
         originals[i].isPrint = true
