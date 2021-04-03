4f7d6f7a4a55af2236f547a5ea253f0da179fb83
Atitayasamaset <72369446+Atitayasamaset@users.noreply.github.com>
2021-04-03 14:49:37 +0700

    update.

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -33,7 +33,7 @@ export const getDebtByInvoice = (req: Request, res: Response) => {
   let list = req.body.list
   let sort = req.body.sort
   let print = req.body.isPrint != undefined ? req.body.isPrint : null
-  Invoice.find({ _id: { $in: list } }).select(sort).then((originals: any) => {
+  Invoice.find({ _id: { $in: list } }).sort(sort).then((originals: any) => {
     let docs = JSON.parse(JSON.stringify(originals))
     Invoice.find({ meter: { $in: docs.map((el: any) => el.meter) } }).sort("-year -month").lean().then((founds: any) => {
       docs.forEach((item: any, i: number) => {
