ba0943d82ea52d089a19d05ff670b6c72111f281
Jirapat Malai <jtm@jmandjm.com>
2021-04-12 17:57:21 +0700

    update.

--- src/import/invoice.ts
+++ src/import/invoice.ts
@@ -8,6 +8,8 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
+let prepArray: Array<any> = [];
+
 (async () => {
   await Invoice.deleteMany({}).exec()
   await Usage.deleteMany({}).exec()
@@ -33,55 +35,68 @@ mongoose.set('useCreateIndex', true);
     { header: 'CategoryType', key: 'categoryType', width: 10 },
   ];
 
-  sheet.eachRow(async function (row: any, rowNumber: number) {
+  sheet.eachRow(function (row: any, rowNumber: number) {
     if (rowNumber > 1) {
       const used = process.memoryUsage().heapUsed / 1024 / 1024;
-      console.log("row", rowNumber)
-      try {
-        await Invoice.create({
-          sequence: row.getCell(2),
-          year: row.getCell(3),
-          month: row.getCell(4),
-          meter: row.getCell(5),
-          name: row.getCell(6),
-          address: row.getCell(7),
-          qty: row.getCell(8),
-          rate: row.getCell(9),
-          flatRate: row.getCell(10),
-          debtText: row.getCell(11),
-          debtAmount: row.getCell(12),
-          totalAmount: row.getCell(13),
-          category: row.getCell(14),
-          categoryType: row.getCell(15),
-        }).then(() => {
-          console.log(`invoice ${rowNumber}: The script uses approximately ${Math.round(used * 100) / 100} MB`);
-        })
-      } catch (error) {
-        console.log("invoice create error", error)
-      }
-      try {
-        await Usage.create({
-          sequence: row.getCell(2),
-          year: row.getCell(3),
-          month: row.getCell(4),
-          meter: row.getCell(5),
-          name: row.getCell(6),
-          address: row.getCell(7),
-          qty: row.getCell(8),
-          rate: row.getCell(9),
-          flatRate: row.getCell(10),
-          debtText: row.getCell(11),
-          debtAmount: row.getCell(12),
-          totalAmount: row.getCell(13),
-          category: row.getCell(14),
-          categoryType: row.getCell(15),
-        }).then(() => {
-          console.log(`usage ${rowNumber}: The script uses approximately ${Math.round(used * 100) / 100} MB`);
-        })
-      } catch (error) {
-        console.log("usage create error", error)
-      }
-      console.log("done", rowNumber)
+      
+      prepArray.push({
+        sequence: row.getCell(2),
+        year: row.getCell(3),
+        month: row.getCell(4),
+        meter: row.getCell(5),
+        name: row.getCell(6),
+        address: row.getCell(7),
+        qty: row.getCell(8),
+        rate: row.getCell(9),
+        flatRate: row.getCell(10),
+        debtText: row.getCell(11),
+        debtAmount: row.getCell(12),
+        totalAmount: row.getCell(13),
+        category: row.getCell(14),
+        categoryType: row.getCell(15),
+      })
+      console.log(`reading ${rowNumber}: Collecting... The script uses approximately ${Math.round(used * 100) / 100} MB`);
     }
   })
+  saveInvoice()
 })()
+var i = 0
+var j = 0
+let saveInvoice = async () => {
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  if (prepArray[i] != undefined) {
+    let invoice = new Invoice(prepArray[i])
+    await invoice.save().then(() => {
+      console.log(`invoices ${i}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`);
+      i++
+      delete mongoose.models['Invoice'];
+      delete mongoose.connection.collections['invoices'];
+      delete mongoose.modelSchemas['Invoice'];
+      setTimeout(() => {
+        saveInvoice()
+      }, 100);
+    })
+  }
+  else{
+    saveUsage()
+  }
+}
+let saveUsage = async () => {
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  if (prepArray[j] != undefined) {
+    let usage = new Usage(prepArray[j])
+    await usage.save().then(() => {
+      console.log(`usages ${j}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`);
+      j++
+      delete mongoose.models['Usage'];
+      delete mongoose.connection.collections['usages'];
+      delete mongoose.modelSchemas['Usage'];
+      setTimeout(() => {
+        saveUsage()
+      }, 100);
+    })
+  }
+  else{
+    console.log('done!',i,j)
+  }
+}
