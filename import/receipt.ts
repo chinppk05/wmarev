ffa86062c804739ab4017b6715e0265709345d21
Jirapat Malai <jtm@jmandjm.com>
2021-04-17 10:34:54 +0700

    update.

--- import/receipt.ts
+++ import/receipt.ts
@@ -68,18 +68,20 @@ let savePayment = async () => {
         delete mongoose.models['Payment'];
         delete mongoose.connection.collections['payments'];
         delete mongoose.modelSchemas['Payment'];
-        try {
-          Invoice.updateOne({ year: prepArray[i].year, month: prepArray[i].month, meter: prepArray[i].meter }, { $set: { isPaid: true, paidReceipt: prepArray[i].sequence } }).then(async (newData: any) => {
-            // setTimeout(() => {
-            let jssl = JSON.stringify({ year: prepArray[i].year, month: prepArray[i].month, meter: prepArray[i].meter })
-            // console.log(`${jssl} - ${(data ?? { sequence: "notfound" }).sequence} ${prepArray[i].year} ${prepArray[i].month} /payments ${i}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`);
-            console.log(`payments ${i}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`)
-            savePayment()
-            i++
-            // }, 1);
-          })
-        } catch (error) {
-          console.log("payment error", error)
+        if (prepArray[i] != undefined) {
+          try {
+            Invoice.updateOne({ year: prepArray[i].year, month: prepArray[i].month, meter: prepArray[i].meter }, { $set: { isPaid: true, paidReceipt: prepArray[i].sequence } }).then(async (newData: any) => {
+              // setTimeout(() => {
+              let jssl = JSON.stringify({ year: prepArray[i].year, month: prepArray[i].month, meter: prepArray[i].meter })
+              // console.log(`${jssl} - ${(data ?? { sequence: "notfound" }).sequence} ${prepArray[i].year} ${prepArray[i].month} /payments ${i}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`);
+              console.log(`payments ${i}: Saving... The script uses approximately ${Math.round(used * 100) / 100} MB`)
+              savePayment()
+              i++
+              // }, 1);
+            })
+          } catch (error) {
+            console.log("payment error", error)
+          }
         }
       })
     })
