52178a76cecd03283be684c1efc53829a8a89232
Atitayasamaset <72369446+Atitayasamaset@users.noreply.github.com>
2021-05-19 17:43:32 +0700

    update.

--- src/controllers/render/index.ts
+++ src/controllers/render/index.ts
@@ -95,7 +95,7 @@ export const postCalculationList = (req: Request, res: Response) => {
             const item = foundCondition.conditions[j-1];
             console.log("j",j)
             if (item.period == "รายไตรมาส") {
-              areaConditions[x].conditions[j].period = "รายไตรมาส"
+              areaConditions[x].conditions[j-1].period = "รายไตรมาส"
               areaConditions[x].save().then((data: any) => console.log("updated"))
             }
             let common = {
