84dfdd6a0f4328d1be594b6352f0771d038e0711
Jirapat Malai <jtm@jmandjm.com>
2021-06-29 11:40:03 +0700

    update.

--- src/routers/auth/index.ts
+++ src/routers/auth/index.ts
@@ -8,7 +8,7 @@ module.exports = (app: Express) => {
   app.post('/api/v1/user/signup/', passport.authenticate('signup', { session: false }), ctrl.signup)
   app.post('/api/v1/user/login/', passport.authenticate('login', { session: false }), ctrl.login)
   app.post('/api/v1/user/logout/', ctrl.logout)
-  app.get('/api/v1/user/list/', ctrl.getUser)
+  app.get('/api/v1/user-list/', ctrl.getUser)
 
   app.post('/api/v1/reset/request/',ctrl.resetRequest)
   app.post('/api/v1/reset/submit/',ctrl.resetSubmission)
