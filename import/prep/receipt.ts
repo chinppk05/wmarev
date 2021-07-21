3750bdf085e8ba126b042c33ec806ebcc7736c10
jirapatmalai <jtm@jmandjm.com>
2021-07-21 14:14:17 +0700

    update.

--- import/prep/receipt.ts
+++ import/prep/receipt.ts
@@ -178,7 +178,11 @@ const moveFrom = "./excel";
                     prep.billAmout = prep.rate * prep.qty
 
                     if(mapper[9]!=undefined){
-                      prep.invoiceAmount = parseFloat(parseFloat(row.getCell(mapper[9]).text).toFixed(2))
+                      if(mapper[9].formula!=undefined){
+                        prep.invoiceAmount = row.getCell(mapper[9]).result
+                      }else{
+                        prep.invoiceAmount = parseFloat((prep.rate * prep.qty).toFixed(2))
+                      }
                     }else{
                       prep.invoiceAmount = parseFloat((prep.rate * prep.qty).toFixed(2))
                     }
