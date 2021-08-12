55d8900f6423cfba58afc68f0de56b37538b2d47
jirapatmalai <jtm@jmandjm.com>
2021-08-12 12:49:37 +0700

    update.

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -197,7 +197,8 @@ export const getGreenYellow = (req: Request, res: Response) => {
         let month = c.recordDate == undefined ? 1 : DateTime.fromISO(c.recordDate).toObject().month
         let year = c.recordDate == undefined ? 1 : DateTime.fromISO(c.recordDate).toObject().year + 543
         // if(month>=10) year = year - 1 
-        return { ...c, month, year, remarkMonth: c.month, remarkYear: c.year, original:c }
+        let bYear = c.year + (c.month>=10?1:0)
+        return { ...c, month, year, remarkMonth: c.month, remarkYear: bYear }
       })
 
       incomes = incomes.map(o => {
