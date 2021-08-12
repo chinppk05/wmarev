a5353be35da314c694fac22bfc5b4d13e5e59acd
jirapatmalai <jtm@jmandjm.com>
2021-08-12 11:45:29 +0700

    update.

--- src/controllers/report/index.ts
+++ src/controllers/report/index.ts
@@ -53,7 +53,7 @@ export const getCollectionStatistic = (req: Request, res: Response) => {
   var end = new Date(budgetYearAD, 12, 30);
   Area.find({ reportIncome: true }).select("_id prefix name contractNumber").then((areas: Array<any>) => {
     promises.push(AreaCollection.find({ recordDate: { $exists: true }, month: { $exists: true }, year: { $exists: true } }).select("area quarter month year recordDate amount createdAt").exec())
-    promises.push(AreaIncome.find({}).select("area quarter month year recordDate amount createdAt").exec())
+    promises.push(AreaIncome.find({}).select("area quarter isDebt month year recordDate amount createdAt").exec())
 
     Promise.all(promises).then((responses) => {
       let prep = JSON.parse(JSON.stringify(areas)) as Array<any>
@@ -185,7 +185,7 @@ export const getGreenYellow = (req: Request, res: Response) => {
   var end = DateTime.fromObject({ year: budgetYear, month: 9, day: 30 }).plus({ month: 3 }).endOf('day').toJSDate();
   Area.find({ reportIncome: true }).select("_id prefix name contractNumber").then((areas: Array<any>) => {
     promises.push(AreaCollection.find({ recordDate: { $gte: start, $lt: end }, month: { $exists: true }, year: { $exists: true } }).select("area quarter month year recordDate amount createdAt").exec())
-    promises.push(AreaIncome.find({}).select("area quarter month year recordDate amount createdAt").exec())
+    promises.push(AreaIncome.find({}).select("area quarter isDebt month year recordDate amount createdAt").exec())
 
     Promise.all(promises).then((responses) => {
       let prep = JSON.parse(JSON.stringify(areas)) as Array<any>
