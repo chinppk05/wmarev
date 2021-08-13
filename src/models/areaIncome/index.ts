3fb6612f675e6cfb9aaf1ecaca5088d9bd3884ca
Jirapat Malai <jtm@jmandjm.com>
2021-08-13 15:50:25 +0700

    update.

--- src/models/areaIncome/index.ts
+++ src/models/areaIncome/index.ts
@@ -18,6 +18,19 @@ const schema = new Schema({
   value: String,
   value2: String,
 
+  remark1:String,
+  remark2:String,
+  remark3:String,
+  remark4:String,
+  text1:String,
+  text2:String,
+  text3:String,
+  text4:String,
+  number1:Number,
+  number2:Number,
+  number3:Number,
+  number4:Number,
+
   createdAt: Date,
   recordDate: Date,
 })
