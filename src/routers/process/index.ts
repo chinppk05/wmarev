9386edae8db0000fb4186fba2b5c4904e504b174
jirapatmalai <jtm@jmandjm.com>
2021-12-03 11:35:18 +0700

    update.

--- src/routers/process/index.ts
+++ src/routers/process/index.ts
@@ -5,11 +5,11 @@ let endpoints = "processes"
 
 module.exports = (app: Express) => {
 
+  app.get(`/api/v1/${endpoint}/test`, ctrl.createReceiptV2)
   // app.get(`/api/v1/${endpoint}/test`, (req,res)=>res.send("test done"))
   app.post(`/api/v1/${endpoint}/create/invoice/`, ctrl.createInvoice)
   app.post(`/api/v1/${endpoint}/print/invoice/`, ctrl.printInvoice)
   app.post(`/api/v1/${endpoint}/create/receipt/`, ctrl.createReceipt)
-  app.post(`/api/v1/${endpoint}/test`, ctrl.createReceiptV2)
   
   app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
   app.post(`/api/v1/${endpoint}/request/receipt/`, ctrl.approvalRequestReceipt)
