1626f49e69cee7eebba090b78e2f326ee71892ee
jirapatmalai <jtm@jmandjm.com>
2021-12-03 11:24:04 +0700

    update.

--- src/routers/process/index.ts
+++ src/routers/process/index.ts
@@ -4,14 +4,14 @@ let endpoint = "process"
 let endpoints = "processes"
 
 module.exports = (app: Express) => {
-  app.post(`/api/v1/${endpoint}/create/invoice`, ctrl.createInvoice)
-  app.post(`/api/v1/${endpoint}/print/invoice`, ctrl.printInvoice)
-  app.post(`/api/v1/${endpoint}/create/receipt`, ctrl.createReceipt)
-  app.post(`/api/v1/${endpoint}/create/receiptv2`, ctrl.createReceiptV2)
+  app.post(`/api/v1/${endpoint}/create/invoice/`, ctrl.createInvoice)
+  app.post(`/api/v1/${endpoint}/print/invoice/`, ctrl.printInvoice)
+  app.post(`/api/v1/${endpoint}/create/receipt/`, ctrl.createReceipt)
+  app.post(`/api/v1/${endpoint}/create/receiptv2/`, ctrl.createReceiptV2)
   
-  app.post(`/api/v1/${endpoint}/print/receipt`, ctrl.printReceipt)
-  app.post(`/api/v1/${endpoint}/request/receipt`, ctrl.approvalRequestReceipt)
-  app.post(`/api/v1/${endpoint}/approve/receipt`, ctrl.approvalApprovedReceipt)
-  app.post(`/api/v1/${endpoint}/sign/receipt`, ctrl.signReceipt)
-  app.post(`/api/v1/${endpoint}/print/receipt`, ctrl.printReceipt)
+  app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
+  app.post(`/api/v1/${endpoint}/request/receipt/`, ctrl.approvalRequestReceipt)
+  app.post(`/api/v1/${endpoint}/approve/receipt/`, ctrl.approvalApprovedReceipt)
+  app.post(`/api/v1/${endpoint}/sign/receipt/`, ctrl.signReceipt)
+  app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
 }
