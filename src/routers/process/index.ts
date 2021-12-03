d2102b92ba3932b344733e977120174b388a58ca
jirapatmalai <jtm@jmandjm.com>
2021-12-03 11:39:25 +0700

    update.

--- src/routers/process/index.ts
+++ src/routers/process/index.ts
@@ -7,13 +7,13 @@ module.exports = (app: Express) => {
 
   app.post(`/api/v1/${endpoint}/test`, ctrl.createReceiptV2)
   // app.get(`/api/v1/${endpoint}/test`, (req,res)=>res.send("test done"))
-  // app.post(`/api/v1/${endpoint}/create/invoice/`, ctrl.createInvoice)
-  // app.post(`/api/v1/${endpoint}/print/invoice/`, ctrl.printInvoice)
-  // app.post(`/api/v1/${endpoint}/create/receipt/`, ctrl.createReceipt)
+  app.post(`/api/v1/${endpoint}/create/invoice/`, ctrl.createInvoice)
+  app.post(`/api/v1/${endpoint}/print/invoice/`, ctrl.printInvoice)
+  app.post(`/api/v1/${endpoint}/create/receipt/`, ctrl.createReceipt)
   
-  // app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
-  // app.post(`/api/v1/${endpoint}/request/receipt/`, ctrl.approvalRequestReceipt)
-  // app.post(`/api/v1/${endpoint}/approve/receipt/`, ctrl.approvalApprovedReceipt)
-  // app.post(`/api/v1/${endpoint}/sign/receipt/`, ctrl.signReceipt)
-  // app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
+  app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
+  app.post(`/api/v1/${endpoint}/request/receipt/`, ctrl.approvalRequestReceipt)
+  app.post(`/api/v1/${endpoint}/approve/receipt/`, ctrl.approvalApprovedReceipt)
+  app.post(`/api/v1/${endpoint}/sign/receipt/`, ctrl.signReceipt)
+  app.post(`/api/v1/${endpoint}/print/receipt/`, ctrl.printReceipt)
 }
