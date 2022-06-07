97954f49963d327ba65f9419d6fed87c4d5157a7
jirapatmalai <jtm@jmandjm.com>
2022-06-07 08:35:29 +0700

    done receipt importer.

--- src/controllers/helper/index.ts
+++ src/controllers/helper/index.ts
@@ -592,8 +592,8 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
     'พย': 11,
     'ธค': 12,
   }
-  let paymentStart = DateTime.fromObject({ day: 1, month: monthStart, year: yearStart - 543 }).startOf('day').toJSDate()
-  let paymentEnd = DateTime.fromObject({ day: 1, month: monthEnd, year: yearEnd - 543 }).endOf('month').endOf('day').toJSDate()
+  let paymentStart = DateTime.fromObject({ day: 15, month: monthStart, year: yearStart - 543 }).startOf('month').startOf('day').minus({hour:7}).toJSDate()
+  let paymentEnd = DateTime.fromObject({ day: 15, month: monthEnd, year: yearEnd - 543 }).endOf('month').endOf('day').minus({hour:7}).toJSDate()
   //{paymentDate:{$gte: ISODate('2021-12-01T00:00:00.000+07:00'), $lte: ISODate('2021-12-31T23:59:59.999+07:00')}}
   // let receipts = await Receipt.find({
   //   paymentDate: {
@@ -601,7 +601,14 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
   //     $lte: paymentEnd
   //   },
   // }).sort({ excelNum: 1 }).exec()
+  console.log({paymentStart,paymentEnd})
   let deleteReceipts = await Receipt.deleteMany({
+    paymentDate: {
+      $gte: paymentStart,
+      $lte: paymentEnd
+    },
+  })
+  let deletePayments = await Payment.deleteMany({
     paymentDate: {
       $gte: paymentStart,
       $lte: paymentEnd
@@ -611,6 +618,7 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
   let path = __dirname + "/receipt_fix_002.xlsx";
   await workbook.xlsx.readFile(path);
   console.log(deleteReceipts)
+  console.log(deletePayments)
 
   let worksheet = await workbook.getWorksheet("Sheet1")
   let preps: Array<any> = []
@@ -659,10 +667,10 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
           debtAmount: row.getCell("H").value,
           debtVat: row.getCell("I").value,
           qty: row.getCell("K").value,
-          totalAmount: row.getCell("P").value,
+          totalAmount: row.getCell("L").value,
           vatRate: 0.07,
           vat: row.getCell("M").value,
-          invoiceAmount: row.getCell("N").value,
+          invoiceAmount: row.getCell("P").value,
           code: "01-kb",
           category,
           categoryType: "บาท/ลบ.ม.",
@@ -673,7 +681,9 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
           isSigned: true,
           process: true,
           createdAt: new Date(),
-          paidType
+          paidType,
+          notes: row.getCell("V").text,
+          ref:paidType
         }
         // console.log({prep})
         preps.push(prep)
@@ -683,10 +693,221 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
     }
   })
   let notfounds: Array<any> = []
+  let c_t1 = 0
+  let c_t2_1 = 0
+  let c_t2_2 = 0
+  let c_t3 = 0
+  let c_t3_1 = 0
+  let c_t3_2 = 0
+  let c_t3_3 = 0
+  let c_t4 = 0
+
+  let c_within = 0
+  let c_cross = 0
+  let c_done = 0
+  let c_called = 0
+
+  let doc_created_payment = 0
+  let doc_created_receipt = 0
+  let createItem = (item: any) => {
+    c_called++
+    return new Promise(async (resolve, reject) => {
+      try {
+        let receipt = new Receipt({ ...item, invoices: item.invoices })
+        await receipt.save()
+        c_done += 1
+        resolve("done")
+      } catch (error) {
+        console.log(item)
+        console.log(error)
+        resolve("done")
+      }
+      try {
+        let payment = new Payment({ ...item, invoices: item.invoices_id })
+        await payment.save()
+        // c_done += 1
+        resolve("done")
+      } catch (error) {
+        console.log(item)
+        console.log(error)
+        resolve("done")
+      }
+    })
+  }
   for (let prep of preps) {
     let year = prep.year
     let month = prep.month
     let hasDash = prep.debtText.search("-") > 0
+    let hasSlash = prep.debtText.search("/") > 0
+    if (prep.paidType === "จ่ายตรงเดือน") {
+      let invoice = await Invoice.findOne({ meter: prep.meter, year: prep.year, month: prep.month }).exec()
+      invoice.isPaid = true
+      invoice.receipts = [prep.sequence]
+      await invoice.save()
+      prep.invoices = [invoice.sequence]
+      prep.invoices_id = [invoice._id]
+      c_t1++
+      await createItem(prep)
+
+    } else if (prep.paidType === "จ่ายไม่ตรงเดือน") {
+      if (hasDash || hasSlash) { // หลายเดือน
+        c_t2_1++
+        let isWithinYear = /([ก-๙]{2}|[ก-๙]{3})-([ก-๙]{2}|[ก-๙]{3})(\d{2})/g.exec(prep.debtText)
+        let isCrossYear = /([ก-๙]{2}|[ก-๙]{3})(\d{2})[-/]([ก-๙]{2}|[ก-๙]{3})(\d{2})/g.exec(prep.debtText)
+        if (isWithinYear !== null) { // หลายเดือนไม่ข้ามปี
+          //@ts-ignore
+          let fromMonth = wmaMonth[isWithinYear[1]]; let toMonth = wmaMonth[isWithinYear[2]];
+          let fromYear = (2500 + parseInt(isWithinYear[3])) - 543
+          let invoices = await Invoice.find({ meter: prep.meter, year: fromYear + 543, month: { $gte: fromMonth, $lte: toMonth } }).exec()
+          for (const invoice of invoices) {
+            invoice.isPaid = true
+            invoice.receipts = [prep.sequence]
+            let invoiceSave = await invoice.save()
+          }
+          prep.invoices = invoices.map((inv: any) => inv.sequence)
+          prep.invoices_id = invoices.map((inv: any) => inv._id)
+          c_within++
+          await createItem(prep)
+        } else if (isCrossYear !== null) { // หลายเดือนข้ามปี
+          //@ts-ignore
+          let fromMonth = wmaMonth[isCrossYear[1]]; let toMonth = wmaMonth[isCrossYear[3]];
+          let fromYear = (2500 + parseInt(isCrossYear[2])) - 543; let toYear = (2500 + parseInt(isCrossYear[4])) - 543
+          let months: Array<any> = []
+          let start = DateTime.fromObject({ year: fromYear, month: fromMonth, day: 1 })
+          let end = DateTime.fromObject({ year: toYear, month: toMonth, day: 28 })
+          while (start < end) {
+            months.push({year:start.year+543,month:start.month})
+            start = start.plus({ months: 1 })
+          }
+          let invoices = await Invoice.find({ meter: prep.meter, $or:months }).exec()
+          for (const invoice of invoices) {
+            invoice.isPaid = true
+            invoice.receipts = [prep.sequence]
+            let invoiceSave = await invoice.save()
+          }
+          c_cross++
+          await createItem(prep)
+        } else { // ไม่เข้าพวก
+          await createItem(prep)
+        }
+
+      } else { // เดือนเดียว
+        let onlyOneMonth = /(.*?)(\d{2})/g.exec(prep.debtText)
+        try {
+          //@ts-ignore
+          let month = wmaMonth[onlyOneMonth[1]]
+          let year = parseInt(onlyOneMonth[2]) + 2500
+          let invoice = await Invoice.findOne({ meter: prep.meter, year: year, month: month }).exec()
+          invoice.isPaid = true
+          invoice.receipts = [prep.sequence]
+          let invoiceSave = await invoice.save()
+          prep.invoices = [invoice.sequence]
+          prep.invoices = [invoice._id]
+        } catch (error) {
+          // notfounds.push(prep)
+        }
+        await createItem(prep)
+        c_t2_2++
+
+      }
+
+    } else if (prep.paidType === "จ่ายรวมเดือนปัจจุบัน") {
+      let isWithinYear = /([ก-๙]{2}|[ก-๙]{3})-([ก-๙]{2}|[ก-๙]{3})(\d{2})/g.exec(prep.debtText)
+      let isCrossYear = /([ก-๙]{2}|[ก-๙]{3})(\d{2})[-/]([ก-๙]{2}|[ก-๙]{3})(\d{2})/g.exec(prep.debtText)
+      let onlyOneMonth = /(.*?)(\d{2})/g.exec(prep.debtText)
+      if (isWithinYear !== null) { // หลายเดือนไม่ข้ามปี
+        //@ts-ignore
+        let fromMonth = wmaMonth[isWithinYear[1]]; let toMonth = wmaMonth[isWithinYear[2]];
+        let fromYear = (2500 + parseInt(isWithinYear[3])) - 543
+        let invoices = await Invoice.find({ meter: prep.meter, year: fromYear + 543, month: { $gte: fromMonth, $lte: toMonth } }).exec()
+        for (const invoice of invoices) {
+          invoice.isPaid = true
+          invoice.receipts = [prep.sequence]
+          let invoiceSave = await invoice.save()
+        }
+        prep.invoices = invoices.map((inv: any) => inv.sequence)
+        prep.invoices_id = invoices.map((inv: any) => inv._id)
+        c_within++
+        c_t3_1++
+        await createItem(prep)
+      } else if (isCrossYear !== null) {
+        //@ts-ignore
+        let fromMonth = wmaMonth[isCrossYear[1]]; let toMonth = wmaMonth[isCrossYear[3]];
+        let fromYear = (2500 + parseInt(isCrossYear[2])) - 543; let toYear = (2500 + parseInt(isCrossYear[4])) - 543
+        let months: Array<any> = []
+        let start = DateTime.fromObject({ year: fromYear, month: fromMonth, day: 1 })
+        let end = DateTime.fromObject({ year: toYear, month: toMonth, day: 28 })
+        while (start < end) {
+          months.push({year:start.year+543,month:start.month})
+          start = start.plus({ months: 1 })
+        }
+        let invoices = await Invoice.find({ meter: prep.meter, $or:months }).exec()
+        for (const invoice of invoices) {
+          invoice.isPaid = true
+          invoice.receipts = [prep.sequence]
+          let invoiceSave = await invoice.save()
+        }
+        await createItem(prep)
+        c_t3_2++
+      } else if (onlyOneMonth !== null) {
+        let onlyOneMonth = /(.*?)(\d{2})/g.exec(prep.debtText)
+        try {
+          //@ts-ignore
+          let month = wmaMonth[onlyOneMonth[1]]
+          let year = parseInt(onlyOneMonth[2]) + 2500
+          let invoice = await Invoice.findOne({ meter: prep.meter, year: year, month: month }).exec()
+          invoice.isPaid = true
+          invoice.receipts = [prep.sequence]
+          let invoiceSave = await invoice.save()
+          prep.invoices = [invoice.sequence]
+          prep.invoices = [invoice._id]
+        } catch (error) {
+          notfounds.push(prep)
+        }
+        await createItem(prep)
+        c_t3_3++
+      } else {
+        notfounds.push(prep)
+        await createItem(prep)
+      }
+      c_t3++
+
+
+    } else {
+      c_t4++
+      await createItem(prep)
+
+    }
+  }
+  console.log({
+    "จ่ายตรงเดือน": c_t1,
+    "จ่ายไม่ตรงเดือน - หลายเดือน": c_t2_1,
+    "จ่ายไม่ตรงเดือน - หลายเดือน - ไม่ข้ามปี": c_within,
+    "จ่ายไม่ตรงเดือน - หลายเดือน - ข้ามปี": c_cross,
+    "จ่ายไม่ตรงเดือน - เดือนเดียว": c_t2_2,
+    "จ่ายรวมเดือนปัจจุบัน": c_t3,
+    "จ่ายรวมเดือนปัจจุบัน - หลายเดือน - ไม่ข้ามปี": c_t3_1,
+    "จ่ายรวมเดือนปัจจุบัน - หลายเดือน - ข้ามปี": c_t3_2,
+    "จ่ายรวมเดือนปัจจุบัน - หลายเดือน - ไม่ตรง format": c_t3_3,
+    "ไม่เข้าพวก": c_t4
+  })
+  console.log({
+    c_all: c_t1 + c_within + c_cross + c_t2_2 + c_t3_1 + c_t3_2 + c_t3_3 + c_t4,
+  })
+  console.log({
+    c_done,
+    c_called
+  })
+  console.log(count)
+  console.log(notfounds.length)
+  res.send("done")
+}
+
+
+/*
+
+
+    
     if (prep.paidType === "จ่ายตรงเดือน") {
       let invoice = await Invoice.findOne({ meter: prep.meter, year: prep.year, month: prep.month }).exec()
       // console.log((invoice??{sequence:"-"}).sequence)
@@ -760,13 +981,9 @@ export const excelReceiptImportV2 = async (req: Request, res: Response) => {
       }
       // console.log({year,month, debtText:prep.debtText, hasDash})
     } else if (prep.paidType === "จ่ายรวมเดือนปัจจุบัน") {
-      notfounds.push({meter:prep.meter, receipt:prep.sequence, debtText:prep.debtText, paidType:prep.paidType})
+      // notfounds.push({meter:prep.meter, receipt:prep.sequence, debtText:prep.debtText, paidType:prep.paidType})
     } else {
 
     }
     // console.log({prep})
-  }
-  console.log(count)
-  console.log(notfounds)
-  res.send("done")
-}
+    */
